494cb29cd8af32e7c83b8689b0652fa8939882c0
Alexander Petrov <57758788+K1ndum@users.noreply.github.com>
2021-10-13 15:57:17 +0300

    feat
    Implemented 3, and 4 tasks

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+
+  if(l == null){
+    return l;
+  }
+
+  while (k == l.value) {
+    l = l.next;
+  } 
+
+  let current = l;
+
+  while(current !== null) {
+    if (k == current.value) {
+      current.value = current.next.value
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return l;
 }
+
